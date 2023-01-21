3cfc273e48a1b82bb6536e40caa010448b9dcef8
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2023-01-21 16:03:00 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.76 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+          int answer = 0;
+
+        Arrays.sort(data,(o1,o2)->{
+            return o1[col-1]!=o2[col-1]? o1[col-1]-o2[col-1]:o2[0]-o1[0];
+        });
+
+        for(int i=row_begin-1;i<row_end;i++){
+            int sum=0;
+            for(int j=0;j<data[i].length;j++){
+                sum+=data[i][j]%(i+1);
+            }
+            answer^=sum;
+        }
+
+        return answer;
+    }
+}

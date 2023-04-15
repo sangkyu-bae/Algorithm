14a7ece46a8c0686b39687c8bce67d57cf55f465
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2023-04-15 17:56:21 +0900

    [level 2] Title: 피로도, Time: 0.16 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,23 +1,40 @@
 import java.util.*;
 class Solution {
-    private int answer = 0;
-    private boolean[] visit;
+//     private int answer = 0;
+//     private boolean[] visit;
+//     public int solution(int k, int[][] dungeons) {
+//         visit = new boolean[dungeons.length];
+
+//         dfs(0, k, dungeons);
+
+//         return answer;
+//     }
+//     private void dfs(int depth, int k, int[][]dungeons){
+//         for(int i = 0; i<dungeons.length; i++){
+//             if(!visit[i]&& dungeons[i][0]<=k){
+//                 visit[i] = true;
+//                 dfs(depth+1, k-dungeons[i][1], dungeons);
+//                 visit[i] = false;
+//             }
+//         }
+//         answer = Math.max(answer, depth);
+//     }
+     boolean[] isVisit;
+    int result=0;
     public int solution(int k, int[][] dungeons) {
-        visit = new boolean[dungeons.length];
-
-        dfs(0, k, dungeons);
-
-        return answer;
+        isVisit=new boolean[dungeons.length];
+        dfs(k,dungeons,0);
+        System.out.println(result);
+        return result;
     }
-    private void dfs(int depth, int k, int[][]dungeons){
-        for(int i = 0; i<dungeons.length; i++){
-            if(!visit[i]&& dungeons[i][0]<=k){
-                visit[i] = true;
-                dfs(depth+1, k-dungeons[i][1], dungeons);
-                visit[i] = false;
+    void dfs(int k,int[][] dungeons,int depth){
+        for(int i=0;i<dungeons.length;i++){
+            if(k>=dungeons[i][0]&&k>=dungeons[i][1]&&!isVisit[i]){
+                isVisit[i]=true;
+                dfs(k-dungeons[i][1],dungeons,depth+1);
+                isVisit[i]=false;
             }
         }
-        answer = Math.max(answer, depth);
-  
+        result=depth>result?depth:result;
     }
 }

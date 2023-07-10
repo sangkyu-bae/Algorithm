97bf6e41c328cd4068266eccb920b57c317d664a
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2023-07-10 18:46:38 +0900

    [level 3] Title: 최고의 집합, Time: 0.02 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int s) {
+               if(n > s) return new int[] {-1};
+        int a = s/n;
+        int b = s%n;
+        int[] answer = new int[n];
+        Arrays.fill(answer,a);
+
+        for(int i=0; i<b;i++){
+            answer[i] += 1;
+        }
+
+        Arrays.sort(answer);
+
+        return answer;
+    }
+}

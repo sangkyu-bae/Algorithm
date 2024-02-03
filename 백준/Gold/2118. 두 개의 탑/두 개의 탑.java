c0656e76c77a35d09a327c0ff654dbafeae81e98
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2024-02-03 16:24:08 +0900

    [Gold V] Title: 두 개의 탑, Time: 664 ms, Memory: 62828 KB -BaekjoonHub

--- 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -1,39 +1,39 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
 
         int[] topArr = new int[N];
         int sum = 0;
-        for(int i = 0;i < N ;i++){
-            int top = sc.nextInt();
-            topArr[i] = top;
-            sum += top;
+        for(int i = 0; i < N ; i++){
+            int num = sc.nextInt();
+            topArr[i] = num;
+            sum += num;
         }
 
+        int index =0 ;
         int checkSum = 0;
-        int pairIndex = 0;
         int answer = 0;
-        int distCounterClockWise = sum;
 
-        for(int i = 0; i < N ;i++){
-            while (distCounterClockWise > checkSum){
-                checkSum +=  topArr[pairIndex];
-                distCounterClockWise -= topArr[pairIndex];
-                pairIndex = (pairIndex+1) % N;
+        for(int i = 0 ; i < N; i++){
+            while (sum > checkSum){
+                checkSum += topArr[index];
+                sum -= topArr[index];
+                index = (index+1)%N;
             }
-            
-            answer = Math.max(answer,distCounterClockWise);
-            distCounterClockWise += topArr[i];
+
+            answer = Math.max(sum,answer);
+            sum += topArr[i];
             checkSum -= topArr[i];
         }
 
         System.out.println(answer);
     }
-
 
 }

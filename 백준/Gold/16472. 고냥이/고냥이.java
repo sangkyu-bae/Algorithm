fea1a44bbfdd7586a47ac483edb822d61d089d36
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2024-07-10 21:36:00 +0900

    [Gold IV] Title: 고냥이, Time: 272 ms, Memory: 21044 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,57 +1,46 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
-    static int cnt = 0;
+    static int index  = 0;
+    static int checkSum = 0;
     static int[] frequency = new int[26];
-    static void increase(char alp){
-        if(frequency[alp - 'a']++ == 0)
-            cnt++;
-    }
 
-    static void decrease(char alp){
-        if(--frequency[alp -'a'] == 0)
-            cnt--;
-    }
-
-    static int getAlphabetCount(int[] alphabetFrequency){
-        int cnt = 0;
-        for(int i = 0; i < alphabetFrequency.length;i++ ){
-            if(alphabetFrequency[i] > 0){
-                cnt++;
-            }
+    public static void increase(char a){
+        if(frequency[ a - 'a'] == 0){
+            checkSum++;
         }
 
-        return cnt;
+        frequency[ a - 'a' ] ++;
     }
 
-    public static void main(String[] args) {
+    public static void decrease(char a){
+        if(--frequency[a - 'a'] == 0){
+            checkSum--;
+        }
+    }
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        String catTalk = sc.next();
-        char[] nyang = catTalk.toCharArray();
-        int L = nyang.length;
+        int n = sc.nextInt();
+        char[] alpArr = sc.next().toCharArray();
 
         int ans = 0;
-        int nextIndex = 0;
 
-        for(int i = 0 ;i < L;i++){
-            while (nextIndex < L){
-                increase(nyang[nextIndex++]);
-                if(cnt > N){
-                    decrease(nyang[--nextIndex]);
+        for(int i = 0 ; i < alpArr.length;i++){
+            while (index < alpArr.length){
+                increase(alpArr[index++]);
+
+                if(checkSum > n){
+                    decrease(alpArr[--index]);
                     break;
                 }
             }
 
-            decrease(nyang[i]);
-            ans = Math.max(ans, nextIndex - i);
+            decrease(alpArr[i]);
+            ans = Math.max(ans,index - i);
         }
 
         System.out.println(ans);
-        
     }
-
 }

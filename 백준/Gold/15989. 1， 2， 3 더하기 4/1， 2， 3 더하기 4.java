3d2e4a91738d667cc6169e3a0719096c70b8530d
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2024-11-23 16:34:38 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 220 ms, Memory: 18680 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] dp = new int[10001][4];
+
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for(int i = 4 ; i < 10001; i++){
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-2][1] + dp[i-2][2];
+            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+        }
+
+        for(int i = 0 ; i < n ;i++){
+            int num = sc.nextInt();
+            int sum = dp[num][1] + dp[num][2] + dp[num][3];
+            System.out.println(sum);
+        }
+    }
+
+}

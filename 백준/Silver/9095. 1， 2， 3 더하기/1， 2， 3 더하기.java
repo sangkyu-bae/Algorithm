c441bb2dbb073ddb17e94480417bc82173a92643
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2025-02-22 19:30:41 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 168 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int N =sc.nextInt();
+        int[] dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i = 1 ; i <=N ; i++) {
+            int S = sc.nextInt();
+
+            for(int j = 4 ; j <= S ; j++){
+                dp[j] = dp[j-1] + dp[j-2] + dp[j-3];
+            }
+
+            System.out.println(dp[S]);
+        }
+    }
+
+}
